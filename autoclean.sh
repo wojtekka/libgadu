b004e61fa499865401ca5b4e80390d656e44c422
speedy <speedy@d0e0d552-48cc-411f-a74d-6ebdfb0732cf>
2003-04-22 18:38:17 +0000

    Initial revision

---
+++ autoclean.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rm -rf config.* autom4te.cache Makefile.in Makefile COPYING INSTALL aclocal.m4 libtool ltmain.sh depcomp install-sh missing mkinstalldirs stamp* configure confdefs.h
---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+aclocal -I m4
+autoheader
+autoconf
+automake --add-missing
+libtoolize --force
+
+test x$NOCONFIGURE = x && ./configure $*
+
