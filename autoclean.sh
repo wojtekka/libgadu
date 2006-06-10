4095e326282769ce97b2ce7bea1c2738fda07ecd
(no author) <(no author)@d0e0d552-48cc-411f-a74d-6ebdfb0732cf>
2006-06-10 20:56:23 +0000

    This commit was manufactured by cvs2svn to create tag 'libgadu-1_6_1'.

--- autoclean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# $Id$
-
-make clean
-rm -rf config.* autom4te.cache Makefile.in Makefile COPYING INSTALL aclocal.m4 libtool 
-rm -rf ltmain.sh depcomp install-sh missing mkinstalldirs stamp* configure confdefs.h ltconfig
-rm -rf src/Makefile src/Makefile.in src/libgadu-config.h src/libgadu-config.h.in
-rm -rf src/.deps
-rm -rf src/libgadu-stdint.h stdint.h src/libgadu.pc
