bfb41ee77e73c15d655dcfd3593af9e0e45a298e
wojtekka <wojtekka@d0e0d552-48cc-411f-a74d-6ebdfb0732cf>
2006-08-19 12:29:57 +0000

    Poprawki budowania.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # $Id$
 
-echo aclocal && aclocal -I m4
-echo autoheader && autoheader
-echo libtoolize && libtoolize --copy --force
-echo automake && automake --add-missing --copy --force --foreign
-echo autoconf && autoconf
+echo aclocal && aclocal -I m4 || exit 1
+echo autoheader && autoheader || exit 1
+echo libtoolize && libtoolize --copy --force || exit 1
+echo automake && automake --add-missing --copy --force --foreign || exit 1
+echo autoconf && autoconf || exit 1
 
 test x$NOCONFIGURE = x && echo configure && ./configure $*
 
