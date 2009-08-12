0447010037b13b026f0ee6819f1d5f2a164ddd65
wojtekka <wojtekka@d0e0d552-48cc-411f-a74d-6ebdfb0732cf>
2009-08-12 22:22:11 +0000

    Dodanie funkcji zwracającej aktualny stan sesji. Usunięcie starego kodu.

--- src/debug.c
+++ src/debug.c
@@ -34,6 +34,7 @@
 #include <string.h>
 
 #include "libgadu.h"
+#include "debug.h"
 
 /**
  * Poziom rejestracji informacji odpluskwiających. Zmienna jest maską bitową
@@ -168,21 +169,17 @@ void gg_debug_dump(struct gg_session *gs, int level, const char *buf, int len)
 
 		sprintf(line, "%.4x: ", i);
 		ofs = 6;
-//		gg_debug_session(sess, level, "%.4x: ", i);
 
 		for (j = 0; j < 16; j++) {
 			if (i + j < len)
 				sprintf(line + ofs, " %02x", (unsigned char) buf[i + j]);
-//				gg_debug_session(sess, level, " %02x", (unsigned char) buf[i + j]);
 			else
 				sprintf(line + ofs, "   ");
-//				gg_debug_session(sess, level, "   ");
 
 			ofs += 3;
 		}
 
 		sprintf(line + ofs, "  ");
-//		gg_debug_session(sess, level, "  ");
 		ofs += 2;
 
 		for (j = 0; j < 16; j++) {
@@ -198,7 +195,6 @@ void gg_debug_dump(struct gg_session *gs, int level, const char *buf, int len)
 			}
 
 			line[ofs++] = ch;
-//			gg_debug_session(sess, level, "%c", ch);
 		}
 
 		line[ofs++] = '\n';
@@ -206,6 +202,81 @@ void gg_debug_dump(struct gg_session *gs, int level, const char *buf, int len)
 
 		gg_debug_session(gs, level, "%s", line);
 	}
+}
+
+/**
+ * Zwraca ciąg z nazwą podanego stanu sesji.
+ *
+ * \param state Stan sesji.
+ *
+ * \return Ciąg z nazwą stanu
+ *
+ * \ingroup debug
+ */
+const char *gg_debug_state(enum gg_state_t state)
+{
+	switch (state) {
+#define GG_DEBUG_STATE(x) case x: return #x;
+	GG_DEBUG_STATE(GG_STATE_IDLE)
+	GG_DEBUG_STATE(GG_STATE_RESOLVING)
+	GG_DEBUG_STATE(GG_STATE_CONNECTING)
+	GG_DEBUG_STATE(GG_STATE_READING_DATA)
+	GG_DEBUG_STATE(GG_STATE_ERROR)
+	GG_DEBUG_STATE(GG_STATE_CONNECTING_HUB)
+	GG_DEBUG_STATE(GG_STATE_CONNECTING_GG)
+	GG_DEBUG_STATE(GG_STATE_READING_KEY)
+	GG_DEBUG_STATE(GG_STATE_READING_REPLY)
+	GG_DEBUG_STATE(GG_STATE_CONNECTED)
+	GG_DEBUG_STATE(GG_STATE_SENDING_QUERY)
+	GG_DEBUG_STATE(GG_STATE_READING_HEADER)
+	GG_DEBUG_STATE(GG_STATE_PARSING)
+	GG_DEBUG_STATE(GG_STATE_DONE)
+	GG_DEBUG_STATE(GG_STATE_LISTENING)
+	GG_DEBUG_STATE(GG_STATE_READING_UIN_1)
+	GG_DEBUG_STATE(GG_STATE_READING_UIN_2)
+	GG_DEBUG_STATE(GG_STATE_SENDING_ACK)
+	GG_DEBUG_STATE(GG_STATE_READING_ACK)
+	GG_DEBUG_STATE(GG_STATE_READING_REQUEST)
+	GG_DEBUG_STATE(GG_STATE_SENDING_REQUEST)
+	GG_DEBUG_STATE(GG_STATE_SENDING_FILE_INFO)
+	GG_DEBUG_STATE(GG_STATE_READING_PRE_FILE_INFO)
+	GG_DEBUG_STATE(GG_STATE_READING_FILE_INFO)
+	GG_DEBUG_STATE(GG_STATE_SENDING_FILE_ACK)
+	GG_DEBUG_STATE(GG_STATE_READING_FILE_ACK)
+	GG_DEBUG_STATE(GG_STATE_SENDING_FILE_HEADER)
+	GG_DEBUG_STATE(GG_STATE_READING_FILE_HEADER)
+	GG_DEBUG_STATE(GG_STATE_GETTING_FILE)
+	GG_DEBUG_STATE(GG_STATE_SENDING_FILE)
+	GG_DEBUG_STATE(GG_STATE_READING_VOICE_ACK)
+	GG_DEBUG_STATE(GG_STATE_READING_VOICE_HEADER)
+	GG_DEBUG_STATE(GG_STATE_READING_VOICE_SIZE)
+	GG_DEBUG_STATE(GG_STATE_READING_VOICE_DATA)
+	GG_DEBUG_STATE(GG_STATE_SENDING_VOICE_ACK)
+	GG_DEBUG_STATE(GG_STATE_SENDING_VOICE_REQUEST)
+	GG_DEBUG_STATE(GG_STATE_READING_TYPE)
+	GG_DEBUG_STATE(GG_STATE_TLS_NEGOTIATION)
+	GG_DEBUG_STATE(GG_STATE_REQUESTING_ID)
+	GG_DEBUG_STATE(GG_STATE_WAITING_FOR_ACCEPT)
+	GG_DEBUG_STATE(GG_STATE_WAITING_FOR_INFO)
+	GG_DEBUG_STATE(GG_STATE_READING_ID)
+	GG_DEBUG_STATE(GG_STATE_SENDING_ID)
+	GG_DEBUG_STATE(GG_STATE_DISCONNECTING)
+	GG_DEBUG_STATE(GG_STATE_CONNECT_HUB)
+	GG_DEBUG_STATE(GG_STATE_CONNECT_PROXY_HUB)
+	GG_DEBUG_STATE(GG_STATE_CONNECT_GG)
+	GG_DEBUG_STATE(GG_STATE_CONNECT_PROXY_GG)
+	GG_DEBUG_STATE(GG_STATE_RESOLVE_HUB_SYNC)
+	GG_DEBUG_STATE(GG_STATE_RESOLVE_HUB_ASYNC)
+	GG_DEBUG_STATE(GG_STATE_RESOLVE_PROXY_SYNC)
+	GG_DEBUG_STATE(GG_STATE_RESOLVE_PROXY_ASYNC)
+	GG_DEBUG_STATE(GG_STATE_RESOLVING_HUB)
+	GG_DEBUG_STATE(GG_STATE_RESOLVING_PROXY)
+
+	// Celowo nie ma default, żeby kompilator wyłapał brakujące stany
+	
+	}
+
+	return NULL;
 }
 
 #else
