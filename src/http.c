73881aaa86bc1f5cd667d4da1708519fa5befa0e
wojtekka <wojtekka@d0e0d552-48cc-411f-a74d-6ebdfb0732cf>
2009-08-30 18:41:52 +0000

    Merge r770.

--- src/http.c
+++ src/http.c
@@ -138,7 +138,7 @@ struct gg_http *gg_http_connect(const char *hostname, int port, int async, const
 			return NULL;
 		}
 
-		if (!(h->fd = gg_connect(&addr_list[0], port, 0)) == -1) {
+		if ((h->fd = gg_connect(&addr_list[0], port, 0)) == -1) {
 			gg_debug(GG_DEBUG_MISC, "// gg_http_connect() connection failed (errno=%d, %s)\n", errno, strerror(errno));
 			gg_http_free(h);
 			free(addr_list);
