ee63918846e773c8a496adda05e97370fe00ab67
wojtekka <wojtekka@d0e0d552-48cc-411f-a74d-6ebdfb0732cf>
2010-03-11 21:48:29 +0000

    From b10675abe71ac1c3f8ad82a18a7827845428e47c Mon Sep 17 00:00:00 2001 From: Bartlomiej Zimon <uzi18@o2.pl> Date: Sat, 26 Dec 2009 17:25:11 +0100 Subject: [PATCH] - resolve relay host and set relay_addr with it's IP
    ---
     branches/1.9/src/libgadu.c |   11 ++++++++++-
      1 files changed, 10 insertions(+), 1 deletions(-)

--- src/libgadu.c
+++ src/libgadu.c
@@ -661,7 +661,7 @@ static int gg_session_callback(struct gg_session *sess)
 struct gg_session *gg_login(const struct gg_login_params *p)
 {
 	struct gg_session *sess = NULL;
-	char *hostname;
+	char *hostname, relay_hostname;
 	int port;
 
 	if (!p) {
@@ -810,6 +810,15 @@ struct gg_session *gg_login(const struct gg_login_params *p)
 		hostname = GG_APPMSG_HOST;
 		port = GG_APPMSG_PORT;
 	}
+
+	relay_hostname = GG_RELAY_HOST;
+	struct in_addr relay_addr;
+	if (gg_gethostbyname_real(relay_hostname, &relay_addr, 0) == -1) {
+		gg_debug(GG_DEBUG_MISC, "// gg_login() relay host \"%s\" not found\n", relay_hostname);
+		sess->relay_addr = INADDR_NONE;
+	}
+	else
+		sess->relay_addr = relay_addr.s_addr;
 
 	if (p->hash_type)
 		sess->hash_type = p->hash_type;
