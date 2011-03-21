59fdd6f5d85c6bcc5d51c9afafb26c9984940d43
wojtekka <wojtekka@d0e0d552-48cc-411f-a74d-6ebdfb0732cf>
2011-03-21 23:05:35 +0000

    Funkcja zwracająca nazwę zdarzenia.

--- include/debug.h
+++ include/debug.h
@@ -22,6 +22,7 @@
 #include "libgadu.h"
 
 const char *gg_debug_state(enum gg_state_t state);
+const char *gg_debug_event(enum gg_event_t event);
 void gg_debug_dump(struct gg_session *sess, int level, const char *buf, size_t len);
 
 #endif /* LIBGADU_DEBUG_H */
