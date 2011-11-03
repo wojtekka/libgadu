4d4f75d1d5543268f54b6e71f22d80763c774f50
wojtekka <wojtekka@d0e0d552-48cc-411f-a74d-6ebdfb0732cf>
2011-11-03 22:55:39 +0000

    --force psuje timestampy i powoduje ciagłe uruchamianie configure.

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,8 @@
 
 echo aclocal && aclocal -I m4 || exit 1
 echo autoheader && autoheader || exit 1
-echo libtoolize && libtoolize --copy --force || exit 1
-echo automake && automake --add-missing --copy --force --foreign || exit 1
+echo libtoolize && libtoolize --copy || exit 1
+echo automake && automake --add-missing --copy --foreign || exit 1
 echo autoconf && autoconf || exit 1
 
 if test x$NOCONFIGURE = x ; then
