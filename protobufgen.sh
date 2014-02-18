af38fc6fb93d4675e28347ad40f8c246722b3b7e
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-02-18 09:03:20 +0100

    Fix protobuf-c < 0.14 warnings

--- protobufgen.sh
+++ protobufgen.sh
@@ -7,5 +7,11 @@ if [ $? != 0 ] ; then
 fi
 
 sed -i 's/<google\/protobuf-c\/protobuf-c.h>/"protobuf.h"/g' packets.pb-c.h
+
+if ! cat packets.pb-c.c | grep "PROTOBUF_C_NO_DEPRECATED" > /dev/null; then
+	echo "Warning: protobuf-c-compiler < 0.14 detected, trying to fix the output"
+	sed -i 's|    NULL,NULL    /\* reserved1, reserved2 \*/|    0, 0, NULL, NULL|g' packets.pb-c.c
+fi
+
 mv packets.pb-c.h include
 mv packets.pb-c.c src
