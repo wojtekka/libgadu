4a6ae5c10024ea4e3552c4d1f73c2265550fdbcf
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-03-01 00:03:43 +0100

    Fix improper gg_mk(s)temp implementation

--- examples/token.c
+++ examples/token.c
@@ -44,7 +44,7 @@ static void sigchld(int sig)
 #endif
 
 static inline int
-gg_mktemp(const char *path)
+gg_mkstemp(char *path)
 {
 #if defined(_BSD_SOURCE) || defined(_SVID_SOURCE) || _XOPEN_SOURCE >= 500
 	return (mkstemp(path) != -1);
@@ -120,7 +120,7 @@ int main(void)
 
 	t = h->data;
 
-	if (!gg_mktemp(path)) {
+	if (!gg_mkstemp(path)) {
 		printf("Błąd tworzenia pliku tymczasowego.\n");
 		gg_token_free(h);
 		return 1;
