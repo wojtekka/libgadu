834ad788de63638f37d8bf1a7eaf66ba61bed4ba
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-05-14 13:32:32 +0200

    Add declspec-conversion script for MSVC

---
+++ declspec.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# $Id$
+
+# this script if for converting libgadu.h file to be used with MSVC
+
+cat $1 \
+	| sed -e "s/^[a-z][a-z0-9_ *]\+gg_[a-z0-9_]\+(.*/__declspec(dllimport) \0/g"\
+	| sed -e "s/^extern [a-z][a-z0-9_ *]\+gg_[a-z0-9_]\+;.*/__declspec(dllimport) \0/g"\
+	| sed -e "s/^extern [a-z][a-z0-9_ *]\+(\*gg_[a-z0-9_]\+)(.*/__declspec(dllimport) \0/g"
