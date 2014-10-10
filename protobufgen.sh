55aecad6404838690ace4b3125f110f812425545
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-10-10 12:30:44 +0200

    Update internal libprotobuf-c to 1.0.2 and require 1.0.0 for build

--- protobufgen.sh
+++ protobufgen.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+if ! protoc-c --version | grep "protobuf-c 1." > /dev/null; then
+	PROTOC_VER=`protoc-c --version | grep protobuf-c | cut -d' ' -f 2`
+	if [ "$PROTOC_VER" = "" ]; then
+		PROTOC_VER="none"
+	fi
+	echo "Invalid protoc-c version. Required 1.x.y, but $PROTOC_VER found."
+	exit -2
+fi
+
 protoc-c --c_out=. packets.proto
 
 if [ $? != 0 ] ; then
 	exit -1
 fi
 
-sed -i 's/<google\/protobuf-c\/protobuf-c.h>/"protobuf.h"/g' packets.pb-c.h
-
-if ! cat packets.pb-c.c | grep "PROTOBUF_C_NO_DEPRECATED" > /dev/null; then
-	echo "Warning: protobuf-c-compiler < 0.14 detected, trying to fix the output"
-	sed -i 's|    NULL,NULL    /\* reserved1, reserved2 \*/|    0, 0, NULL, NULL|g' packets.pb-c.c
-fi
+sed -i 's/<protobuf-c\/protobuf-c.h>/"protobuf.h"/g' packets.pb-c.h
 
 mv packets.pb-c.h include
 mv packets.pb-c.c src
