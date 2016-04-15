52afe5ac64014124800493d9fd594c0487f36976
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2016-04-15 15:57:37 +0100

    Set DLL path to fix some Win64 tests

--- windows/build.sh
+++ windows/build.sh
@@ -2,7 +2,14 @@
 
 export PKG_CONFIG_PATH=/usr/i686-w64-mingw32/sys-root/mingw/lib/pkgconfig:/usr/i686-w64-mingw32/sys-root/mingw/share/pkgconfig
 export PKG_CONFIG=/usr/bin/i686-w64-mingw32-pkg-config
-export WINEPREFIX="/.wine32"
+export WINEPREFIX="/.wine"
+
+if [ -n "${WINE_SET_PATH}" ]; then
+	echo -e 'REGEDIT4\n\n[HKEY_CURRENT_USER\\Environment]' > /wine-set-path.reg
+	echo "\"PATH\"=\"${WINE_SET_PATH}\"" >> /wine-set-path.reg
+	wine regedit /S /wine-set-path.reg
+	rm -f /wine-set-path.reg
+fi
 
 export LIBGADU_FLAGS="--host=${TARGET} --target=${TARGET} CC=${TARGET}-gcc \
 	--enable-shared --disable-static --with-c99-vsnprintf --with-pthread \
