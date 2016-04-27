46d9e9d5dcc9d2380e7adfec75989e939f18ca0d
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2016-04-27 14:12:06 +0100

    Coverity build: set package version as 'version' and git revision as 'description'

--- coverity/build.sh
+++ coverity/build.sh
@@ -4,11 +4,12 @@
 cov-build --dir cov-int make check
 tar cjf /artifacts/libgadu-cov.tar.bz2 cov-int
 
-VERSION=$(git rev-parse --short HEAD)
+GIT_REVISION=$(git rev-parse --short HEAD)
 
 curl --fail \
 	--form token=${COVERITY_TOKEN} \
 	--form email=${COVERITY_EMAIL} \
 	--form file=@/artifacts/libgadu-cov.tar.bz2 \
-	--form version="${VERSION}" \
+	--form version="${PKG_VERSION}" \
+	--form description="Git rev ${GIT_REVISION}" \
 	https://scan.coverity.com/builds?project=wojtekka%2Flibgadu
